a4dd25ed3169c48bf3f5c6c0afb2caa941013a7a
catherine244 <ccayreen24@gmail.com>
2020-01-13 12:20:31 +0300

    ran my tests and worked

--- app/email.py
+++ app/email.py
@@ -5,7 +5,7 @@ from . import mail
 
 
 def email_message(subject, template, to, **kwargs):
-    sender_email = 'waichigovick@gmail.com'
+    sender_email = 'ccayreen24@gmail.com'
     email = Message(subject, sender=sender_email, recipients=[to])
     email.body = render_template(template + ".txt", **kwargs)
     email.html = render_template(template + ".html", **kwargs)
--- manage.py
+++ manage.py
@@ -22,7 +22,7 @@ def add_shell_context():
 @manager.command
 def test():
     import unittest
-    tests = unittest.TestLoader().discover('tests')
+    tests = unittest.TestLoader().discover('test')
     unittest.TextTestRunner(verbosity=2).run(tests)
 
 
