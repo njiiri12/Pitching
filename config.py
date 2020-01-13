d355d8b15a02117f4022705f0ea124e42e83e871
catherine244 <ccayreen24@gmail.com>
2020-01-13 15:50:04 +0300

    added config options

--- config.py
+++ config.py
@@ -6,8 +6,7 @@ import os
 class Config:
     debug = True
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://cate:love1234@localhost/love'
 
     #  email configurations
     MAIL_SERVER = 'smtp.gmail.com'
@@ -16,6 +15,32 @@ class Config:
     MAIL_USE_SSL = True
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    
+    
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    pass
+
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+
+    DEBUG = True
+
+config_options = {
+    "development": DevConfig,
+    "production": ProdConfig
+}    
 
 
 
